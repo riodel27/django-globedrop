ae692c8c1b0002bd9351b9c0d2678076fda51b67
rpamaong <rpamaong@startechup.com>
2020-09-01 19:31:00 +0800

    modify user fieldset on admin site

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from .models import User
 
@@ -10,6 +11,25 @@ class CustomUserAdmin(UserAdmin):
     model = User
     list_display = ('username', 'email', 'name', 'user_type',
                     'language', 'created_at', 'updated_at')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': (
+            'name', )}),
+        (_('Additional info'), {'fields': (
+            "language", "user_type", )
+        }),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'name', 'email', 'password1', 'password2', 'language', 'user_type'), }),)
+
     ordering = ['created_at']
 
 
